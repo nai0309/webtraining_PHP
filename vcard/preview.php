d63c920d1728fbd58088cb6c3c7d1ccbad8abd08
nai0309 <nainai0309@gmail.com>
2019-12-02 23:49:54 +0800

    版型調整.1

--- vcard/preview.php
+++ vcard/preview.php
@@ -3,8 +3,9 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
     <?php
     session_start();
     ?>
@@ -58,6 +59,13 @@
             </div>
         </div>
     </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.3.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
 </body>
 
 </html>
