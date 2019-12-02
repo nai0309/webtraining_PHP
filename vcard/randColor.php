37409af68772ce70e7a2e9a3ba4c086f9fd556de
nai0309 <nainai0309@gmail.com>
2019-12-02 23:49:29 +0800

    隨機配色版型_完成

--- vcard/randColor.php
+++ vcard/randColor.php
@@ -16,12 +16,24 @@
         <!-- css reset -->
     <link rel="stylesheet" href="css/reset.css" />
     <!-- css樣式 -->
-    <link rel="stylesheet" href="css/<?= $_SESSION['data']['vcardstyle'] ?>">
+    <link rel="stylesheet" href="css/randColor.css">
 
 
 </head>
+<?php
+$bgCol = "rgb(" . rand(150, 255) . "," . rand(150, 255) . "," . rand(150, 255) . ")";
+$picBorder = "rgb(" . rand(0, 255) . "," . rand(0, 255) . "," . rand(0, 255) . ")";
+$itemCol = "rgb(" . rand(0, 150) . "," . rand(0, 150) . "," . rand(0, 150) . ")";
 
-<body>
+$rand=rand(1,9);
+if($rand<4){$borderStyle='solid';}
+elseif($rand<7){$borderStyle='dotted';}
+else{$borderStyle='dashed';}
+
+// background color
+echo '<body style="background:'.$bgCol.'">';
+?>
+
     <!-- 彈性盒 -->
     <div class="main">
         <span class="bgName"><?= $_SESSION['data']['name'] ?></span>
@@ -31,26 +43,42 @@
                 <img src="img_upload/<?= $_SESSION['data']['pic'] ?>">
                 <!-- 名字 -->
                 <div class="name">
-                    <span class="item">ABOUT.</span>
+                <?php
+                echo '<span class="item" style="color:'.$itemCol.'">ABOUT.</span>';
+                ?>
+                    <span class="item"></span>
                     <span class="abName"><?= $_SESSION['data']['name'] ?></span>
                 </div>
+                <?php
+               echo '<div class="picBack" style="border: 5px '.$borderStyle.' '.$picBorder.';"></div>';
+               ?>
             </div>
+            
             <div class="text">
                 <!-- 個人資料 -->
-                <span class="item">Occupation.</span><br>
+                <?php
+                echo '<span class="item" style="color:'.$itemCol.'">Occupation.</span><br>';
+                ?>
                 <span class="content"><?= $_SESSION['data']['job'] ?></span>
                 <br>
-                <span class="item">Mobile.</span><br>
+                <?php
+                echo '<span class="item" style="color:'.$itemCol.'">Mobile.</span><br>';
+                ?>
                 <span class="content"><?= $_SESSION['data']['phoneNum'] ?></span>
                 <br>
-                <span class="item">E-mail.</span><br>
+                <?php
+                echo '<span class="item" style="color:'.$itemCol.'">E-mail.</span><br>';
+                ?>
                 <span class="content"><a href="mailto:<?= $_SESSION['data']['mail'] ?>"><?= $_SESSION['data']['mail'] ?></a></span>
                 <br>
-                <span class="item">Ability.</span><br>
+                <?php
+                echo '<span class="item" style="color:'.$itemCol.'">Ability.</span><br>';
+                ?>
                 <span class="content"><?= $_SESSION['data']['skills'] ?></span>
             </div>
         </div>
     </div>
-</body>
-
+<?php
+echo '</body>';
+?>
 </html>
